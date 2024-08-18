be959b533f44338d463c42e9c29cfc1585d933ba
MuradGitHub <github@nayal.org>
2024-08-17 20:25:26 -0400

    Updated robot config. on an Automonous opmod

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorImpl;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class TankTeleOp extends LinearOpMode {
@@ -18,12 +15,12 @@ public class TankTeleOp extends LinearOpMode {
     @Override
     public void runOpMode(){
 
-        DcMotor drive1 = hardwareMap.dcMotor.get("drive1");
-        DcMotor drive2 = hardwareMap.dcMotor.get("drive2");
+        DcMotor drive1 = hardwareMap.dcMotor.get("drive_left");
+        DcMotor drive2 = hardwareMap.dcMotor.get("drive_right");
         drive1.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        DcMotor arm1 = hardwareMap.dcMotor.get("arm1");
-        DcMotor arm2 = hardwareMap.dcMotor.get("arm2");
+        DcMotor arm1 = hardwareMap.dcMotor.get("arm_left");
+        DcMotor arm2 = hardwareMap.dcMotor.get("arm_right");
         arm2.setDirection(DcMotorSimple.Direction.REVERSE);
 
         Servo claw = hardwareMap.servo.get("claw");
